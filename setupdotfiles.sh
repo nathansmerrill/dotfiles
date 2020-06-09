be5f62d7be08bcfed96c0aadaa8c7b1db759dbf9
Nathan Merrill <48145856+nathansmerrill@users.noreply.github.com>
2020-06-08 18:54:16 -0700

    Update setupdotfiles.sh

--- setupdotfiles.sh
+++ setupdotfiles.sh
@@ -1,7 +1,9 @@
 echo "Setting up environment"
 
+echo "Installing packages"
+sudo apt install -y git zsh vim
+
 echo "Setting up shell"
-chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -10,11 +12,13 @@ echo "Downloading dotfiles"
 curl -o ~/.zshrc https://raw.githubusercontent.com/nathansmerrill/dotfiles/master/.zshrc.remote
 curl -o ~/.p10k.zsh https://raw.githubusercontent.com/nathansmerrill/dotfiles/master/.p10k.zsh
 curl -o ~/.vimrc https://raw.githubusercontent.com/nathansmerrill/dotfiles/master/.vimrc
+sed -i "s/<USERNAME>/$USER/" ~/.zshrc
 
 echo "Setting up vim"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
 
 echo "Installing rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 cargo install bat exa
